c5a741634be5f17412d822a0a658da2de4eeda1c
Denis <grabsomebuds27@gmail.com>
2020-05-09 11:23:37 +0300

    dev-layout 0.0

---
+++ Doomarket/gulpfile.js
@@ -0,0 +1,54 @@
+// Компилятор с обновлением страницы
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const sourcemaps = require('gulp-sourcemaps');
+// const watch = require('gulp-watch');
+
+//Компилируем css в scss
+function style() {
+	return gulp.src('./scss/**/*.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('./css/'))
+		.pipe(browserSync.stream());
+}
+
+function watch() {
+	browserSync.init({
+		server: {
+			baseDir: './'
+		}
+	});
+	gulp.watch('./scss/**/*.scss', style);
+	gulp.watch('./*.html/').on('change', browserSync.reload);
+	gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
+
+
+
+
+
+// Просто компилятор
+// const gulp = require('gulp');
+// const sass = require('gulp-sass');
+// const sourcemaps = require('gulp-sourcemaps');
+// const watch = require('gulp-watch');
+// const browserSync = require('browser-sync').create();
+
+// gulp.task('sass-compile', function(){
+// 	return gulp.src('./scss/**/*.scss',)
+// .pipe(sourcemaps.init())
+// .pipe(sass().on('error', sass.logError))
+// .pipe(sourcemaps.write('./'))
+// .pipe(gulp.dest('./css/'))
+
+// })
+
+// gulp.task('watch', function(){
+// 	gulp.watch('./scss/**/*.scss', gulp.series('sass-compile'))
+// })
