90876e060dfef4694ec6e88db3e9d66b60eab185
Denis <grabsomebuds27@gmail.com>
2020-05-29 20:40:50 +0300

    adaptive+login+register+wishlist

--- Doomarket/js/wish-list.js
+++ Doomarket/js/wish-list.js
@@ -1,11 +1,11 @@
 window.addEventListener("DOMContentLoaded", function() {
 	let products = document.querySelectorAll(".card"),
-	buttons = document.querySelectorAll(".card__icon-btn"),
-	open = document.getElementsByClassName("header-link__text")[1],
-	close = document.getElementsByClassName("close")[0];
+		buttons = document.querySelectorAll(".card__icon-btn"),
+		buttonsOnDeleteFromWL = document.querySelectorAll(".card__icon-btn---hover"),
+		open = document.getElementsByClassName("header-link__text")[1];
+		
 
-
-	let block = document.querySelector(".wishlist__block-wrapper")
+	let block = document.querySelector(".wishlist__block-wrapper"),
 		field = document.querySelector(".wishlist__container"),
 		cart = document.querySelector(".wishlist__block"),
 		close = document.querySelector(".close");
@@ -15,14 +15,28 @@ window.addEventListener("DOMContentLoaded", function() {
 		// click для каждой из кнопок в списке товаров
 		buttons[i].addEventListener("click", function() {
 			// Копия для корзины + удаление кнопки (добавления в избранное) из главной страницы и из самого избранного
-			let item  = products[i].cloneNode(true);
-				btn = item.querySelector("button")
+			let item  = products[i].cloneNode(true),
+				btn = item.querySelector("button");
 				
 			btn.remove()		
 			field.appendChild(item);
 			buttons[i].remove();
 		})
 	}
+
+	// // Функция удаления товара из избранного
+	// for (let i = 0; i < buttonsOnDeleteFromWL.length; i++) {
+	// 	// click для каждой из кнопок в списке товаров
+	// 	buttonsOnDeleteFromWL[i].addEventListener("click", function() {
+	// 		// let itemOnDelete  = products[i],
+	// 		let buttonsHover = products[i].querySelector("button");
+				
+	// 		buttonsHover.remove();	
+	// 		field.remove(products[i]);
+	// 		buttonsOnDeleteFromWL[i].remove();
+	// 		products[i].append(buttons[i]);
+	// 	})
+	// }
 
 	
 	// Функция открытия избранного
