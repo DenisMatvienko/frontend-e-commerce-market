1f4ae608d6ff6fd4f0652f178efe8011cd2b642e
Denis <grabsomebuds27@gmail.com>
2020-06-16 16:26:46 +0300

    pages+modified

--- Doomarket/gulpfile.js
+++ Doomarket/gulpfile.js
@@ -28,27 +28,3 @@ function watch() {
 
 exports.style = style;
 exports.watch = watch;
-
-
-
-
-
-// Просто компилятор
-// const gulp = require('gulp');
-// const sass = require('gulp-sass');
-// const sourcemaps = require('gulp-sourcemaps');
-// const watch = require('gulp-watch');
-// const browserSync = require('browser-sync').create();
-
-// gulp.task('sass-compile', function(){
-// 	return gulp.src('./scss/**/*.scss',)
-// .pipe(sourcemaps.init())
-// .pipe(sass().on('error', sass.logError))
-// .pipe(sourcemaps.write('./'))
-// .pipe(gulp.dest('./css/'))
-
-// })
-
-// gulp.task('watch', function(){
-// 	gulp.watch('./scss/**/*.scss', gulp.series('sass-compile'))
-// })
